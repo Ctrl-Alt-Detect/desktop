a29015c065975569cb923bd113841b8493c3dcef
DMax-YT <13312994+DMax-YT@users.noreply.github.com>
2025-12-18 16:32:09 +0300

    camera chooser

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,22 +7,38 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    QCamera *camera = new QCamera(this);
+    QCamera *camera = nullptr;
     QCameraViewfinder *viewfinder = new QCameraViewfinder(this);
     viewfinder->setSizePolicy(QSizePolicy::Maximum,QSizePolicy::Maximum);
-    camera->setViewfinder(viewfinder);
-
     ui->groupBox->layout()->addWidget(viewfinder);
 
-    camera->start(); // to start the viewfinder
+    ui->chooseCameraList->addItem("Выберите камеру", QVariant());
 
-
-    int i = 0;
+    int i = 1;
     const QList<QCameraInfo> cameras = QCameraInfo::availableCameras();
     for (const QCameraInfo &cameraInfo : cameras)
     {
-        ui->chooseCameraList->addItem(cameraInfo.description(), i++);
+        ui->chooseCameraList->addItem(cameraInfo.description(), QVariant::fromValue(cameraInfo));
     }
+
+    connect(ui->chooseCameraList, QOverload<int>::of(&QComboBox::currentIndexChanged), [=, &camera](int index)
+    {
+        if (index == 0)
+        {
+            if (camera == nullptr) return;
+
+            camera->stop();
+            camera->deleteLater();
+            camera = nullptr;
+            return;
+        }
+
+        QCameraInfo selectedInfo = ui->chooseCameraList->itemData(index).value<QCameraInfo>();
+        // Now you can create a QCamera using selectedInfo
+        camera = new QCamera(selectedInfo, this);
+        camera->setViewfinder(viewfinder);
+        camera->start();
+    });
 }
 
 MainWindow::~MainWindow()
